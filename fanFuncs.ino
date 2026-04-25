02f836541f6651e2ad035d56315af7ff8936606a
johanbogg <johan@bogg.nu>
2026-04-25 23:23:02 +0200

    Fix bugs: fanFuncs.ino

--- fanFuncs.ino
+++ fanFuncs.ino
@@ -1,21 +1,15 @@
-
-
 void setFanSpeed(int reqSpeed) {
-  if ( reqSpeed == fanMode) {} // If new speed is same as current speed, do nothing
-  else {  // Else, if current and new speed aren't the same, turn all pins to low and set the correct pin to high.
-    digitalWrite(LOW_SPDPIN, LOW); 
-    digitalWrite(MID_SPDPIN, LOW); 
-    digitalWrite(HIGH_SPDPIN, LOW);
-    delay(250);
-    if (reqSpeed == 0) { }
-  else if (reqSpeed == 1) { digitalWrite(LOW_SPDPIN, HIGH); }
-    
-  else if (reqSpeed == 2) {   digitalWrite(MID_SPDPIN, HIGH); }
-  
-  else if (reqSpeed == 3) { digitalWrite(HIGH_SPDPIN, HIGH);  }
-  
-  else { }
-  
-  fanMode=reqSpeed;
-}
+  if (reqSpeed == fanMode) return;
+
+  // Turn all relays off first, then activate the requested speed
+  digitalWrite(LOW_SPDPIN,  LOW);
+  digitalWrite(MID_SPDPIN,  LOW);
+  digitalWrite(HIGH_SPDPIN, LOW);
+  delay(250);
+
+  if      (reqSpeed == 1) { digitalWrite(LOW_SPDPIN,  HIGH); }
+  else if (reqSpeed == 2) { digitalWrite(MID_SPDPIN,  HIGH); }
+  else if (reqSpeed == 3) { digitalWrite(HIGH_SPDPIN, HIGH); }
+
+  fanMode = reqSpeed;
 }
