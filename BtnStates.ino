68ab57ede8ee2c6985de085483850f57ea0c2704
johanbogg <johan@bogg.nu>
2026-04-25 23:22:56 +0200

    Fix bugs: BtnStates.ino

--- BtnStates.ino
+++ BtnStates.ino
@@ -1,140 +1,89 @@
 void chkBtns() {
-  
-  selBtnstate=digitalRead(SEL_BTN);
-  upBtnstate=digitalRead(UP_BTN);
-  dwnBtnstate=digitalRead(DWN_BTN);
 
-// check SelBtn
+  selBtnstate = digitalRead(SEL_BTN);
+  upBtnstate  = digitalRead(UP_BTN);
+  dwnBtnstate = digitalRead(DWN_BTN);
+
+  // SEL button
   if (selBtnstate == HIGH && upBtnstate == LOW && dwnBtnstate == LOW) {
-
-    if (oldselBtnstate == 1) { oldselBtnstate = 1;}
-    else { 
-      oldselBtnstate = 1; 
-      if (displayMode == 0) {      
-      if (heatMode == 0) { heatMode=1; checkTemp(tempSensor1,tempSensor2,lowTemp);}
-      else if (heatMode == 1) { heatMode=2; setFanSpeed(0);}
-      else if (heatMode == 2) { heatMode=0; checkTemp(tempSensor1,tempSensor2,lowTemp);}
+    if (oldselBtnstate == 0) {
+      oldselBtnstate = 1;
+      if (displayMode == 0) {
+        if (heatMode == 0)      { heatMode = 1; checkTemp(tempSensor1, tempSensor2, lowTemp); }
+        else if (heatMode == 1) { heatMode = 2; setFanSpeed(0); }
+        else if (heatMode == 2) { heatMode = 0; checkTemp(tempSensor1, tempSensor2, lowTemp); }
       }
       else if (displayMode == 1) {
-        if (menuSelVal >= 5) { menuSelVal=0; }
+        if (menuSelVal >= 5) { menuSelVal = 0; }
         else { menuSelVal++; }
-        }
-      else if (displayMode == 2) { displayMode=0; }
+        // Skip Save when nothing has changed
+        if (menuSelVal == 4 && settingsChanged == 0) { menuSelVal = 5; }
+      }
+      else if (displayMode == 2) { displayMode = 0; }
     }
-    }
-    
-// check UpBtn
-   if (upBtnstate == HIGH && dwnBtnstate == LOW && dwnBtnstate == LOW) {
+  }
 
+  // UP button
+  if (upBtnstate == HIGH && selBtnstate == LOW && dwnBtnstate == LOW) {
     if (displayMode == 0 && heatMode == 2) {
-if (oldupBtnstate == 1) {}
-      else {
-      newfanMode=fanMode;
-      if ( fanMode <= 2 ) { newfanMode++; setFanSpeed(newfanMode);}
-      oldupBtnstate = 1;
+      if (oldupBtnstate == 0) {
+        oldupBtnstate = 1;
+        if (fanMode <= 2) { setFanSpeed(fanMode + 1); }
       }
-      }
-   if (displayMode == 1) {
-    if (oldupBtnstate == 1) { oldupBtnstate = 1;}
-    else { 
-      oldupBtnstate = 1; 
-      if (menuSelVal==0) {
-      lowTemp++;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==1) {
-      midTemp++;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==2) {
-      highTemp++;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==3) {
-      maxiterations++;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==4 && settingsChanged == 1) {
-      SaveSettings();
-      }
-      else if (menuSelVal==5) {
-      displayMode=0;
-      }
-      }
-         }
-   }
-   
-   
-// check DwnBtn
-
-   if (dwnBtnstate == HIGH && upBtnstate == LOW && selBtnstate == LOW) {
-    if (displayMode == 0 && heatMode == 2) { 
-      if (olddwnBtnstate == 1) {}
-      else {
-      newfanMode=fanMode;
-      if ( newfanMode >= 1 ) { newfanMode--; setFanSpeed(newfanMode); }
-      olddwnBtnstate = 1;
-      }
-      }
+    }
     if (displayMode == 1) {
-    if (olddwnBtnstate == 1) { olddwnBtnstate = 1;}
-    else { 
-      olddwnBtnstate = 1; 
-      if (menuSelVal==0) {
-      lowTemp--;
-      settingsChanged=1;
+      if (oldupBtnstate == 0) {
+        oldupBtnstate = 1;
+        if      (menuSelVal == 0) { lowTemp++;      settingsChanged = 1; }
+        else if (menuSelVal == 1) { midTemp++;      settingsChanged = 1; }
+        else if (menuSelVal == 2) { highTemp++;     settingsChanged = 1; }
+        else if (menuSelVal == 3) { maxiterations++; settingsChanged = 1; }
+        else if (menuSelVal == 4) { SaveSettings(); }
+        else if (menuSelVal == 5) { displayMode = 0; }
       }
-      else if (menuSelVal==1) {
-      midTemp--;
-      
-      settingsChanged=1;
-      }
-      else if (menuSelVal==2) {
-      highTemp--;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==3) {
-      maxiterations--;
-      settingsChanged=1;
-      }
-      else if (menuSelVal==4 && settingsChanged == 1) {
-      SaveSettings();
-      }
-      else if (menuSelVal==5) {
-      displayMode=0;
-      }
-      }
-         }
-      
     }
-// check if upBtn and sel is pressed at the same time:
-  
+  }
+
+  // DOWN button
+  if (dwnBtnstate == HIGH && upBtnstate == LOW && selBtnstate == LOW) {
+    if (displayMode == 0 && heatMode == 2) {
+      if (olddwnBtnstate == 0) {
+        olddwnBtnstate = 1;
+        if (fanMode >= 1) { setFanSpeed(fanMode - 1); }
+      }
+    }
+    if (displayMode == 1) {
+      if (olddwnBtnstate == 0) {
+        olddwnBtnstate = 1;
+        if      (menuSelVal == 0) { lowTemp--;      settingsChanged = 1; }
+        else if (menuSelVal == 1) { midTemp--;      settingsChanged = 1; }
+        else if (menuSelVal == 2) { highTemp--;     settingsChanged = 1; }
+        else if (menuSelVal == 3) { maxiterations--; settingsChanged = 1; }
+        else if (menuSelVal == 4) { SaveSettings(); }
+        else if (menuSelVal == 5) { displayMode = 0; }
+      }
+    }
+  }
+
+  // UP + SEL: toggle menu
   if (upBtnstate == HIGH && selBtnstate == HIGH && dwnBtnstate == LOW) {
-    
-    if (oldselBtnstate==1 && oldupBtnstate==1) { }
-    else if (displayMode==0) {
-    displayMode=1;
-    oldupBtnstate = 1;
-    oldselBtnstate = 1;
-    }
-    else if (displayMode==1) {
-      displayMode=0;
-      oldupBtnstate = 1;
+    if (oldselBtnstate == 0 || oldupBtnstate == 0) {
+      oldupBtnstate  = 1;
       oldselBtnstate = 1;
+      displayMode = (displayMode == 0) ? 1 : 0;
     }
-    
   }
-  if (upBtnstate == HIGH && selBtnstate == LOW && dwnBtnstate == HIGH) { 
-    if (olddwnBtnstate == 1 && oldupBtnstate == 1) {    }
-    else if ( displayMode == 0) { 
-      displayMode=2;
+
+  // UP + DWN: show version screen
+  if (upBtnstate == HIGH && dwnBtnstate == HIGH && selBtnstate == LOW) {
+    if (olddwnBtnstate == 0 || oldupBtnstate == 0) {
       olddwnBtnstate = 1;
-      oldupBtnstate = 1;
-      }
-    } 
-  
-    if (dwnBtnstate == LOW) { olddwnBtnstate = 0; }
-    if (selBtnstate == LOW) { oldselBtnstate = 0; }
-    if (upBtnstate == LOW) { oldupBtnstate = 0; } 
-  
+      oldupBtnstate  = 1;
+      if (displayMode == 0) { displayMode = 2; }
+    }
   }
+
+  if (dwnBtnstate == LOW) { olddwnBtnstate = 0; }
+  if (selBtnstate == LOW) { oldselBtnstate = 0; }
+  if (upBtnstate  == LOW) { oldupBtnstate  = 0; }
+}
